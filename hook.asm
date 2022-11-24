1ad666bdf44a93b34ce2188d9cd4829f6437e6c2
aglab2 <aglab3@gmail.com>
2022-11-24 13:56:41 +0800

    Convert centiseconds patch to asm

--- hook.asm
+++ hook.asm
@@ -14,34 +14,17 @@
 ; Centiseconds
 .orga 0x9EA63
 	.db 0xB4
+
 .orga 0x9E9C8
-	.dh 0x2401
-.orga 0x9E9CA
-	.dh 0x000A
+	ADDIU AT, R0, 0xA
+
 .orga 0x9EA00
-	.dh 0x0321
-.orga 0x9EA02 
-	.dh 0x0018
-.orga 0x9EA04 
-	.dh 0x0000
-.orga 0x9EA06 
-	.dh 0x4812
-.orga 0x9EA08 
-	.dh 0x2401
-.orga 0x9EA0A 
-	.dh 0x0003
-.orga 0x9EA0C 
-	.dh 0x0121
-.orga 0x9EA0E 
-	.dh 0x001A
-.orga 0x9EA10 
-	.dh 0x0000
-.orga 0x9EA12 
-	.dh 0x5012
-.orga 0x9EA14 
-	.dh 0xA7AA
-.orga 0x9EA16 
-	.dh 0x0024
+	MULT T9, AT
+	MFLO T1
+	ADDIU AT, R0, 3
+	DIV T1, AT
+	MFLO T2
+	SH T2, 0x24(SP)
 	
 .orga 0x4688
 	SB K1, 0xB25E(AT)
@@ -120,17 +103,17 @@
 .definelabel code_end_copy, 0xEE00+code_rom
 
 .orga 0x396c; here it copies code into the ram at start up
-li a0, code_ram
-li a1, code_rom
-li.u a2, code_end_copy
-jal 0x80278504
-li.l a2, code_end_copy
-jal execonce
-nop
+	li a0, code_ram
+	li a1, code_rom
+	li.u a2, code_end_copy
+	jal 0x80278504
+	li.l a2, code_end_copy
+	jal execonce
+	nop
 
 .orga 0xfd354
-jal execeveryframe
-nop
+	jal execeveryframe
+	nop
 
 ; FIXME: crashes on VC
 ;.orga 0xde270; responsible for running every VI frames
@@ -155,75 +138,71 @@ nop
 .headersize (code_ram - code_rom)
 
 .orga code_rom
-execonce:
-addiu sp, sp, -0x18; don't get rid of those extra things. they are needed
-sw ra, 0x14(sp)
-or a0, r0, r0
-jal 0x80277ee0
-lui a1, 0x8000
-lui a0, 0x8034
-lui a1, 0x8034
-addiu a1, a1, 0xb044
-addiu a0, a0, 0xb028
-jal 0x803225a0
-addiu a2, r0, 0x1
-lw ra, 0x14(sp)
-jr ra
-addiu sp, sp, 0x18
-nop
-nop
-nop
-nop
-nop
-nop
-nop
-nop
+	execonce:
+	addiu sp, sp, -0x18; don't get rid of those extra things. they are needed
+	sw ra, 0x14(sp)
+	or a0, r0, r0
+	jal 0x80277ee0
+	lui a1, 0x8000
+	lui a0, 0x8034
+	lui a1, 0x8034
+	addiu a1, a1, 0xb044
+	addiu a0, a0, 0xb028
+	jal 0x803225a0
+	addiu a2, r0, 0x1
+	lw ra, 0x14(sp)
+	jr ra
+	addiu sp, sp, 0x18
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
 
 .orga code_rom+0x100
-execeveryframe:
-addiu sp, sp, 0xffe8; don't get rid of those extra things. they are needed
-sw ra, 0x14(sp)
+	execeveryframe:
+	addiu sp, sp, 0xffe8; don't get rid of those extra things. they are needed
+	sw ra, 0x14(sp)
 
-lw at, 0x8004e000
-jalr at
-nop
+	lw at, 0x8004e000
+	jalr at
+	nop
 
-; -------------------------------------------------------------------------------------- every frame
+	addiu t6, r0, 0x1; don't get rid of those extra things. they are needed
+	lui at, 0x8039
+	lw ra, 0x14(sp)
+	jr ra
+	addiu sp, sp, 0x18
 
-addiu t6, r0, 0x1; don't get rid of those extra things. they are needed
-lui at, 0x8039
-lw ra, 0x14(sp)
-jr ra
-addiu sp, sp, 0x18
-
-nop
-nop
-nop
-nop
-nop
-nop
-nop
-nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
 
 ;execviframes:
-;addiu sp, sp, 0xffe8
-;sw ra, 0x18(sp)
-;sw s5, 0x14(sp)
+	;addiu sp, sp, 0xffe8
+	;sw ra, 0x18(sp)
+	;sw s5, 0x14(sp)
 
-;lw at, 0x8004e004
-;jalr at
-;nop
+	;lw at, 0x8004e004
+	;jalr at
+	;nop
 
-; -------------------------------------------------------------------------------------- every vi frame here
+	; -------------------------------------------------------------------------------------- every vi frame here
 
-;lw ra, 0x18(sp)
-;addiu sp, sp, 0x18
-;jr ra
-;lw ra, 0x14(sp)
+	;lw ra, 0x18(sp)
+	;addiu sp, sp, 0x18
+	;jr ra
+	;lw ra, 0x14(sp)
 
-;nop
-;nop
-;nop
-;nop
-
-code_end:
+	;nop
+	;nop
+	;nop
+	;nop
